007725dca347ab00944343723363956cf0dde91f
Sharlock93 <sharo93@gmail.com>
2025-01-20 23:59:32 +0000

    meow

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 set libs_win=user32.lib gdi32.lib OpenGL32.lib
-cl /nologo -Iinclude src\main.c  /Fe./build/main.exe -Fo./build/main.obj
+cl /nologo -Iinclude src\main.c  /Zi /Fe./build/main.exe -Fo./build/main.obj
